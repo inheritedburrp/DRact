fd95e12f44e3958022a3a8ec5b82619452ce27dc
Anuj Sharma <anujsharma92_8@live.com>
2020-09-01 00:52:19 +0200

    login and register page added to React and route accordingly

--- DR/frontend/src/components/App.js
+++ DR/frontend/src/components/App.js
@@ -1,15 +1,18 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
 
 import Dashboard from './users/Dashboard';
 import Header from './layout/Header';
+import Alerts from './layout/Alerts';
+import Login from './accounts/Login';
+import Register from './accounts/Register';
 
 import { Provider } from 'react-redux';
 import store from '../store';
-import Alerts from './layout/Alerts';
 
 //Alert Options 
 const alertOptions = {
@@ -22,13 +25,19 @@ class App extends Component {
         return (
             <Provider store={store}>
                 <AlertProvider template={AlertTemplate} {...alertOptions}>
-                    <Fragment>
-                        <Header />
-                        <Alerts />
-                        <div className='container'>
-                            <Dashboard />
-                        </div>
-                    </Fragment>
+                    <Router>
+                        <Fragment>
+                            <Header />
+                            <Alerts />
+                            <div className='container'>
+                                <Switch>
+                                    <Route exact path="/" component={Dashboard} />
+                                    <Route exact path="/register" component={Register} />
+                                    <Route exact path="/login" component={Login} />
+                                </Switch>
+                            </div>
+                        </Fragment>
+                    </Router>
                 </AlertProvider>
             </Provider>
         );
